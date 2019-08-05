75e6b596b39c6cfdc1528a33f92253af07271299
Maksym <mfedorenko16@gmail.com>
2019-08-05 14:09:08 -0400

    ls method

--- bash.js
+++ bash.js
@@ -1,10 +1,12 @@
 const pwd = require('./pwd');
+const ls = require('./ls');
 
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
   if (cmd === 'pwd') return pwd();
+  if (cmd === 'ls') return ls();
   process.stdout.write('You typed: ' + cmd);
   process.stdout.write('\nprompt > ');
 
--- pwd.js
+++ pwd.js
@@ -4,14 +4,4 @@ module.exports = function (cmd) {
 }
 
 
-// process.stdin.on('data', (data) => {
-//   const cmd = data.toString().trim();
-//   if (cmd === 'pwd') {
-//     process.stdout.write(process.cwd());
-//   }
-//   else {
-//   process.stdout.write('You typed: ' + cmd);
-//   }
-//   process.stdout.write('\nprompt > ');
 
-// });
